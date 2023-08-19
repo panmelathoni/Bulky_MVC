604567acd6ac3371d6f641d61a7ed84616144be3
Fabio Oliveira da Silva <fabiofos@gmail.com>
2023-08-19 16:02:16 +0100

    ViewModel ProductVM created and implemented

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -35,6 +35,7 @@ namespace Bulky.DataAccess.Data
                    Price50 = 85,
                    Price100 = 80,
                    CategoryId = 1,
+                   ImageUrl=""
                },
                 new Product
                 {
@@ -48,6 +49,7 @@ namespace Bulky.DataAccess.Data
                     Price50 = 25,
                     Price100 = 20,
                     CategoryId = 1,
+                    ImageUrl = ""
                 },
                 new Product
                 {
@@ -61,6 +63,7 @@ namespace Bulky.DataAccess.Data
                     Price50 = 40,
                     Price100 = 35,
                     CategoryId = 1,
+                    ImageUrl = ""
                 },
                 new Product
                 {
@@ -74,6 +77,7 @@ namespace Bulky.DataAccess.Data
                     Price50 = 60,
                     Price100 = 55,
                     CategoryId = 2,
+                    ImageUrl = ""
                 },
                 new Product
                 {
@@ -87,6 +91,7 @@ namespace Bulky.DataAccess.Data
                     Price50 = 25,
                     Price100 = 20,
                     CategoryId = 3,
+                    ImageUrl = ""
                 },
                 new Product
                 {
@@ -100,6 +105,7 @@ namespace Bulky.DataAccess.Data
                     Price50 = 22,
                     Price100 = 20,
                     CategoryId = 3,
+                    ImageUrl = ""
                 }
               );
         }
--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
@@ -17,28 +19,47 @@ namespace BulkyWeb.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+
+      
             return View(objProductList);
         }
 
         public IActionResult Create()
-        {
-            return View();
+        {          
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }),
+                Product = new Product()
+            };
+            return View(productVM);
         }
 
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM productVM)
         {
            
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["Success"] = "Product created successfully";
-
                 return RedirectToAction("Index");
             }
-            return View(obj);
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                });
+               
+            }
+            return View(productVM);
 
         }
 
